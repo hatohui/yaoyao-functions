3e7ef687c1a7a562e06fbcaad8ad3dd9efc090ef
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2026-07-18 04:09:52 +0700

    fix: add lambda permission resource for public URL access

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -38,4 +38,12 @@ resource "aws_lambda_function_url" "yaoyao_function_url" {
   authorization_type = "NONE"
 }
 
+resource "aws_lambda_permission" "allow_public_url" {
+  statement_id           = "AllowPublicAccess"
+  action                 = "lambda:InvokeFunctionUrl"
+  function_name          = aws_lambda_function.yaoyao_function.function_name
+  principal              = "*"
+  function_url_auth_type = "NONE"
+}
+
 
